483b81416d33acd0c0124800503ecdfa75b8cec0
Christian Muise <christian.muise@gmail.com>
2016-01-25 12:13:49 +0000

    Finished work on the testing page.

--- client/js/test.js
+++ client/js/test.js
@@ -4,23 +4,34 @@ function run_tests() {
 
     $('#go-button').prop('disabled', true);
 
-    var PIDs = {1:8, 2:8, 3:8, 4:8, 5:8, 6:8, 7:8, 8:8, 9:8, 10:8};
+    // Random selection of 10 problems from different domains.
+    var PIDs = {1:8, 2:2352, 3:1809, 4:2703, 5:1116, 6:4399, 7:211, 8:4436, 9:818, 10:2487};
 
-    //solve(PIDs[1], function(resp1) {show_result('test1', resp1)})
-
+    // Want to call each test one after another.
     var cb = function(resp) {show_result('test10', resp)};
-    for (var i=9; i>=1; --i) {
-        cb = function(resp) {show_result('test'+i, resp); solve(PIDs[i+1], cb)};
+    for (var i=Object.keys(PIDs).length-1; i>=1; i--) {
+        // We need to wrap in an anonymous function so the index is evaluated
+        cb = (function(ind, cb) {
+            return function(resp) {
+                show_result('test'+ind, resp);
+                solve_and_validate(PIDs[ind+1], cb);
+            };
+        })(i, cb);
     }
-    solve(PIDs[1], cb);
+
+    solve_and_validate(PIDs[1], cb);
 }
 
 function show_result(lid, resp) {
     console.log(resp);
+    if (resp.result.val_status === "valid")
+        $('#'+lid).append("<strong>Ok!</strong>");
+    else
+        $('#'+lid).append("<strong>Failed: "+resp.result.val_status+"</strong>");
 }
 
 function solve_and_validate(pid, cb) {
-
+    query('solve-and-validate', {probID:pid}, cb);
 }
 
 function solve(pid, cb) {
